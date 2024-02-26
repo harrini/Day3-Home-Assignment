e4fa1fe594bc34628c2d7e12d4ef89b12f9638a4
harrini <harrini.rp@gmail.com>
2024-02-26 12:16:19 +0530

    Add files via upload

--- salesForceLogin.spec.ts
+++ salesForceLogin.spec.ts
@@ -9,16 +9,17 @@ test('Login with Salesforce' , async({page})=> {
     console.log(await page.url);
 
     //To enter the Login value
-    await page.fill("#username", "vidyar@testleaf.com");
+    await page.fill("#username", "harrini.rp@yahoo.com");
 
     //To enter the Password value
-    await page.fill("#password", "Testleaf@1234");    
+    await page.fill("#password", "Shiridi@1288");    
 
     //To click on Login button
     await page.click("#Login");
-
     await page.waitForTimeout (3000);
-
+    //To pick the title
+    console.log(await page.url());
+   
 
 
 
